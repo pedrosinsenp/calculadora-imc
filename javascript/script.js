b9678887d3d16a2a0fa56cb81682aeb1c551c4c4
pedrosinsenp <142235199+pedrosinsenp@users.noreply.github.com>
2023-09-22 15:24:53 -0300

    upadate

--- javascript/script.js
+++ javascript/script.js
@@ -11,11 +11,11 @@ function calcular() {
         if (imc < 17) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Muito abaixo do peso!!'
-            seu_estado.style.color = 'darkred'
+            seu_estado.style.color = '#bb0b0b'
         } else if (imc < 18.5) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Abaixo do peso!'
-            seu_estado.style.color = '#bf2604'
+            seu_estado.style.color = '#c44d4d'
         } else if (imc < 25) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Peso normal'
@@ -23,19 +23,19 @@ function calcular() {
         } else if (imc < 30) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Acima do peso'
-            seu_estado.style.color = '#bf2604'
+            seu_estado.style.color = '#c44d4d'
         } else if (imc < 35) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Obesidade grau 1!'
-            seu_estado.style.color = 'red'
+            seu_estado.style.color = '#be2929'
         } else if (imc < 40) {
             seu_imc.innerHTML = imc
             seu_estado.innerHTML = 'Obesidade grau 2!!'
-            seu_estado.style.color = 'darkred'
+            seu_estado.style.color = '#bb0b0b'
         } else if (imc >= 40) {
             seu_imc.innerHTML = imc
-            seu_estado.innerHTML = 'Obesidade grau 3 (mórbida)!!!'
-            seu_estado.style.color = '#260101'
+            seu_estado.innerHTML = 'Obesidade grau 3!!!'
+            seu_estado.style.color = '#a20202'
         }
     } else {
         seu_imc.innerHTML = ''
